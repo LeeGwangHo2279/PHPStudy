116d7cd387268db478ff852a018ee7c56d4a1a35
이광호 <dlrhkdgh2279@gmail.com>
2022-08-01 23:38:12 +0900

    생성자( 인스턴스 초기화 )

--- class_instance.php
+++ class_instance.php
@@ -8,6 +8,9 @@
 //3. method를 만들때는 class안에 정의되어있는 함수를 사용하면되고,
 // 호출할때는 $file과 화살표를 사용하면 가능하다 
 class MyFileObject{
+    function __construct($fname){ // 생성자 표현 방법
+        $this->filename = $fname;
+    }
     function isFile(){
         //this를 안써주면 그냥 함수안에서의 변수라고만 정의가 된다. (바깥쪽으로 나갈수 없음)
         //$this-> 란 인스턴스에 주입한 파일이름을 여기에요 라고 표시해주는것
@@ -16,16 +19,12 @@ class MyFileObject{
     }
 }
 //구체적인 상품을 만드는것이 인스턴스 
-$file = new MyFileObject();
-$file->filename = 'data.txt';
+$file = new MyFileObject('data.txt');
+//$file->filename = 'data.txt';
 //여기서 isFile()은 메소드
 var_dump($file->isFile());
 var_dump($file->filename);
 
-$file2 = new MyFileObject();
-$file2 ->filename = 'data2.txt';
-var_dump($file2->isFile());
-var_dump($file2->filename);
 
 /*
 MyFileObject : Class
